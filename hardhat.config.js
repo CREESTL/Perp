4d318413391dff2c97aae1bec655412a5cf15912
IntegralTeam <vzverev@sfxdx.com>
2022-04-11 12:49:16 +0700

    Initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,17 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+ require('dotenv').config();
+ const { REPORT_GAS } = process.env;
+ require('@nomiclabs/hardhat-ethers');
+ require('solidity-coverage');
+ require('hardhat-gas-reporter');
+ require("@nomiclabs/hardhat-etherscan");
+ 
+ module.exports = {
+   gasReporter: {
+     enabled: REPORT_GAS === "true" ? true : false
+   },
+   solidity: "0.8.12",
+ };
+ 
