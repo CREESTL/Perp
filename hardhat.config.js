bf0ad5a66f1927055bcbdbd98ba697ffbe1a19f0
Roman Tentser <rtentser@sfxdx.com>
2022-04-14 14:13:18 +0700

    Enable optimizer

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,17 +1,37 @@
 /**
  * @type import('hardhat/config').HardhatUserConfig
- */
- require('dotenv').config();
- const { REPORT_GAS } = process.env;
- require('@nomiclabs/hardhat-ethers');
- require('solidity-coverage');
- require('hardhat-gas-reporter');
- require("@nomiclabs/hardhat-etherscan");
- 
- module.exports = {
-   gasReporter: {
-     enabled: REPORT_GAS === "true" ? true : false
-   },
-   solidity: "0.8.12",
- };
+*/
+require('dotenv').config();
+const { REPORT_GAS } = process.env;
+require('@nomiclabs/hardhat-ethers');
+require('solidity-coverage');
+require('hardhat-gas-reporter');
+require("@nomiclabs/hardhat-etherscan");
+
+module.exports = {
+  networks: {
+    hardhat: {
+    },
+    rinkeby: {
+      url: `https://eth-rinkeby.alchemyapi.io/v2/oRFHpW926CHVKUjXbSuD9oLG4EQLfS9u`,
+      chainId: 4,
+      gasPrice: 7000000000,
+      accounts: {mnemonic: "escape asthma range van match denial settle maze daughter angle curve portion"}
+    },
+  },
+  gasReporter: {
+    enabled: REPORT_GAS === "true" ? true : false
+  },
+  solidity: {
+    compilers: [{
+      version: "0.8.12",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200
+        }
+      }
+    }]
+  }
+};
  
