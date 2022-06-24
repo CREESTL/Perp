aaa8ad9b94dd5c1d80d021f473c27db8af20792e
Roman Tentser <rtentser@sfxdx.com>
2022-06-24 17:20:35 +0700

    Update interface

--- contracts/interfaces/ITrading.sol
+++ contracts/interfaces/ITrading.sol
@@ -6,6 +6,7 @@ interface ITrading {
     function distributeFees(address currency) external;
     
     function settleOrder(address user, bytes32 productId, address currency, bool isLong, uint256 price) external;
+    function settleLimit(address user, bytes32 productId, address currency, bool isLong, uint256 price) external;
 
     function liquidatePosition(address user, bytes32 productId, address currency, bool isLong, uint256 price) external;
 
